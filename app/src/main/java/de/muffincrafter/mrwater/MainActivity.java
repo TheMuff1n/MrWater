50113afba0bcbb028abbe763cc5550b8ca996ae7
Marvin <muffinprogramming@gmail.com>
2017-11-12 00:46:34 +0100

    Added navigation drawer

--- app/src/main/java/de/muffincrafter/mrwater/MainActivity.java
+++ app/src/main/java/de/muffincrafter/mrwater/MainActivity.java
@@ -1,50 +1,111 @@
 package de.muffincrafter.mrwater;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import java.util.List;
-
-import android.view.inputmethod.InputMethodManager;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import android.util.SparseBooleanArray;
-import android.view.ActionMode;
-import android.widget.AbsListView;
-
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.widget.TextView;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import java.util.ArrayList;
 import android.support.v4.app.Fragment;
 import de.muffincrafter.mrwater.Fragments.ProductsFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.os.PersistableBundle;
+import android.content.res.Configuration;
 
 public class MainActivity extends AppCompatActivity
 {
 	public static final String LOG_TAG = MainActivity.class.getSimpleName();
-	
+
+	private ActionBarDrawerToggle drawerToggle;
+	private DrawerLayout drawerLayout;
+	private String activityTitle;
+
+	private ListView drawerList;
+	private ArrayAdapter<String> adapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		activityTitle = getTitle().toString();
+		setupDrawer();
+
+		drawerList = (ListView) findViewById(R.id.navList);
+		initializeDrawer();
+
 		getSupportFragmentManager().beginTransaction()
-		.replace(R.id.fragment_container, new ProductsFragment())
-		.commit();
+			.replace(R.id.fragment_container, new ProductsFragment())
+			.commit();
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState)
+	{
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig)
+	{
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (drawerToggle.onOptionsItemSelected(item))
+		{
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void setupDrawer()
+	{
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+												 R.string.drawer_open, R.string.drawer_close) {
+			public void onDrawerOpened(View drawerView)
+			{
+				super.onDrawerOpened(drawerView);
+				getSupportActionBar().setTitle(getString(R.string.navigation));
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerClosed(View drawerView)
+			{
+				super.onDrawerClosed(drawerView);
+				getSupportActionBar().setTitle(activityTitle);
+				invalidateOptionsMenu();
+			}
+		};
+		drawerToggle.setDrawerIndicatorEnabled(true);
+
+		drawerLayout.addDrawerListener(drawerToggle);
+	}
+
+	private void initializeDrawer()
+	{
+		String[] fragmentsArray = getResources().getStringArray(R.array.fragments_string_array);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fragmentsArray);
+		drawerList.setAdapter(adapter);
+
+		drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+				{
+					Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_LONG).show();
+				}
+			});
 	}
 }
